8ec7708fd00e6c7983c79e33709db48013e012f3
Sahar L <saharl@menteebot.com>
2026-03-23 14:35:12 +0200

    readibility changes

--- msh_2_xdmf.py
+++ msh_2_xdmf.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 import meshio
 from pathlib import Path
 import re
@@ -5,88 +8,70 @@ from collections import defaultdict
 import pandas as pd
 
 
-def msh_to_xdmf(path: Path):
-    # Make sure to export msh in ASCII 2 format! otherwise wont work.
-    # when meshing in gmsh, dont forget to make physical groups of all boundaris,
-    # and of the inner volume!
-
-    # Load the .msh file
-    msh = meshio.read(path)
-
-    # Extract cells and data for the volume (Tetrahedrons)
-    # If your mesh is 2D, change 'tetra' to 'triangle'
-    tetra_mesh = meshio.Mesh(
-        points=msh.points,
-        cells={"tetra": msh.cells_dict["tetra"]},
-        cell_data={"name_to_read": [msh.cell_data_dict["gmsh:physical"]["tetra"]]},
-    )
-
-    # Write to XDMF
-    meshio.write(path.with_suffix(".xdmf"), tetra_mesh)
 
 
-def msh_to_xdmf2(path: Path):
-    msh = meshio.read(path)
-
-    # 1. Extract and save the 3D Volume (The Mesh)
-    # This includes the tetrahedrons (Type 4)
-    tetra_mesh = meshio.Mesh(
-        points=msh.points, cells={"tetra": msh.cells_dict["tetra"]}
-    )
-    path_vol = path.with_stem(path.stem + "_volume").with_suffix(".xdmf")
-    meshio.write(path_vol, tetra_mesh)
-
-    # 2. Extract and save the 2D Facets (The Boundary Tags)
-    # This includes the triangles (Type 2) and their Physical Group IDs
-    facet_mesh = meshio.Mesh(
-        points=msh.points,
-        cells={"triangle": msh.cells_dict["triangle"]},
-        cell_data={"name_to_read": [msh.cell_data_dict["gmsh:physical"]["triangle"]]},
-    )
-    path_surface = path.with_stem(path.stem + "_surface").with_suffix(".xdmf")
-    meshio.write(path_surface, facet_mesh)
+def msh_to_xdmf(path: Path, to_save: Path = None) -> None:
+    """convert the msh file to xdmf and h5 files that can be read by fenicsx.
 
 
-def msh_to_xdmf_universal(path: Path):
+    Parameters
+    ----------
+    path : Path
+        the path to the .msh file to be converted.
+    to_save: Path, default to None
+        new place to save the files to. defaults to the same folder.
+    """
+
+
     check_geo_duplicate_physical_groups(path.with_suffix(".geo"))
-    msh = meshio.read(path)
 
-    # Determine which 3D elements exist
-    if "hexahedron" in msh.cells_dict:
+
+    msh = meshio.read(path)
+    vol_type, surface_type = determine_elements_type(msh)
+
+
+    save_volume_elements(msh, vol_type)
+    save_surface_elements(msh, surface_type)
+
+
+
+
+def determine_elements_type(mesh) -> Tuple[str, str]:
+    """detects the type of elements in the msh file. whether they are hexagonal or tetraheadral
+
+
+    Parameters
+    ----------
+    mesh : meshio.mesh.Mesh
+        the loaded mesh from the .msh file.
+
+
+    Returns
+    -------
+    Tuple[str, str]
+        volumetric type and surface type of the mesh.
+    """
+    if "hexahedron" in mesh.cells_dict:
         vol_type, surf_type = "hexahedron", "quad"
     else:
         vol_type, surf_type = "tetra", "triangle"
+    return vol_type, surf_type
 
-    # 1. Extract and save the 3D Volume (The Mesh)
-    # This includes the tetrahedrons (Type 4)
-    vol_mesh = meshio.Mesh(
-        points=msh.points,
-        cells={vol_type: msh.cells_dict[vol_type]},
-        cell_data={"Grid": [msh.cell_data_dict["gmsh:physical"][vol_type]]},
-    )
-    path_vol = path.with_stem(path.stem + "_volume").with_suffix(".xdmf")
-    meshio.write(path_vol, vol_mesh)
 
-    # 2. Extract and save the 2D Facets (The Boundary Tags)
-    # This includes the triangles (Type 2) and their Physical Group IDs
-    facet_mesh = meshio.Mesh(
-        points=msh.points,
-        cells={surf_type: msh.cells_dict[surf_type]},
-        cell_data={"Grid": [msh.cell_data_dict["gmsh:physical"][surf_type]]},
-    )
-    path_surface = path.with_stem(path.stem + "_surface").with_suffix(".xdmf")
-    meshio.write(path_surface, facet_mesh)
 
 
 def check_geo_duplicate_physical_groups(path):
     # Regex to find Physical Surface("Name") = {ID1, ID2, ...};
     pattern = r'Physical (Surface|Volume)\("([^"]+)",\s*(\d+)?\)\s*=\s*\{([^}]+)\};'
 
+
     surface_to_groups = defaultdict(list)
+
 
     with open(path, "r") as f:
         content = f.read()
         matches = re.findall(pattern, content)
+
 
         for group_type, group_name, group_num, ids_str in matches:
             # Clean up the IDs (remove spaces and split by comma)
@@ -95,6 +80,7 @@ def check_geo_duplicate_physical_groups(path):
                 surface_to_groups[f"{s_id}, ({group_type})"].append(
                     [group_name, group_num]
                 )
+
 
         duplicate_faces = [
             [s_id, groups]
@@ -109,8 +95,41 @@ def check_geo_duplicate_physical_groups(path):
             )
 
 
+
+
+def save_volume_elements(mesh, vol_type):
+    # 1. Extract and save the 3D Volume (The Mesh)
+    # This includes the tetrahedrons (Type 4)
+    vol_mesh = meshio.Mesh(
+        points=mesh.points,
+        cells={vol_type: mesh.cells_dict[vol_type]},
+        cell_data={"Grid": [mesh.cell_data_dict["gmsh:physical"][vol_type]]},
+    )
+    path_vol = path.with_stem(path.stem + "_volume").with_suffix(".xdmf")
+    meshio.write(path_vol, vol_mesh)
+
+
+
+
+def save_surface_elements(mesh, surface_type):
+    # 2. Extract and save the 2D Facets (The Boundary Tags)
+    # This includes the triangles (Type 2) and their Physical Group IDs
+    facet_mesh = meshio.Mesh(
+        points=mesh.points,
+        cells={surface_type: mesh.cells_dict[surface_type]},
+        cell_data={"Grid": [mesh.cell_data_dict["gmsh:physical"][surface_type]]},
+    )
+    path_surface = path.with_stem(path.stem + "_surface").with_suffix(".xdmf")
+    meshio.write(path_surface, facet_mesh)
+
+
+
+
 if __name__ == "__main__":
     path = Path(
         r"/mnt/c/Users/saharl/Documents/V3.2/hand/finger_heat_transfer/fin_assembly/fin_asm2.msh"
     )
-    msh_to_xdmf_universal(path)
+    msh_to_xdmf(path)
+
+
+
